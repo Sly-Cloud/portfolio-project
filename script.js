30faf88e64a1d7685b9d786193588eb24eaa1d22
olawale-o <omoogunolawale@gmail.com>
2021-06-21 15:35:25 +0100

    Add hamburgerTag aboutTag closeTag contactTag portfolioTag and mainTag variables to script.js File

---
+++ script.js
@@ -0,0 +1,6 @@
+const hamburgerTag = document.querySelector('#hamburger');
+const aboutTag = document.querySelector('#about');
+const closeTag = document.querySelector('#close');
+const contactTag = document.querySelector('#contact');
+const portfolioTag = document.querySelector('#portfolio');
+const mainTag = document.querySelector('main');
