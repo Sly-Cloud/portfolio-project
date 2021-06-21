f829d9905f5c7c34e9ae213025327449fc1389c9
Sylvester <nnaemekasilver@gmail.com>
2021-06-21 15:58:50 +0100

    Add behaviour for hamburger

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 const hamburgerTag = document.querySelector('#hamburger');
-const aboutTag = document.querySelector('#about');
 const closeTag = document.querySelector('#close');
 const contactTag = document.querySelector('#contact');
+const aboutTag = document.querySelector('#about');
 const portfolioTag = document.querySelector('#portfolio');
 const mainTag = document.querySelector('main');
+
+hamburgerTag.addEventListener('click', () => {
+  mainTag.classList.add('active');
+});
+
+closeTag.addEventListener('click', () => {
+  mainTag.classList.remove('active');
+});
+
+portfolioTag.addEventListener('click', () => {
+  mainTag.classList.toggle('active');
+});
+
+aboutTag.addEventListener('click', () => {
+  mainTag.classList.toggle('active');
+});
+
+contactTag.addEventListener('click', () => {
+  mainTag.classList.toggle('active');
+});
